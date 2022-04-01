ddf28bc2390ff9365dfe6564c258f9a68cff729b
Mickael Lokhate <lokhatemickael@gmail.com>
2022-04-01 14:51:20 +0200

    WIP oauth2

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:oauth2/oauth2.dart' as oauth2;
+
+Future main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  oauth2.Client? client;
 
   @override
   Widget build(BuildContext context) {
+    _intraAuthorization();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -16,5 +23,17 @@ class MyApp extends StatelessWidget {
       ),
       home: const Center(child: Text('Swifty Companion'),),
     );
+  }
+
+  void _intraAuthorization() async {
+    final authorizationEndpoint = Uri.parse('https://api.intra.42.fr/oauth/authorize');
+    final uid = dotenv.env['FT_UID'];
+    final secret = dotenv.env['FT_SECRET'];
+    final credentialsFile = File('~/.swifty/cred.json');
+
+    client = await oauth2.clientCredentialsGrant(authorizationEndpoint, uid, secret);
+    if (client != null) {
+      await credentialsFile.writeAsString(client!.credentials.toJson());
+    }
   }
 }
